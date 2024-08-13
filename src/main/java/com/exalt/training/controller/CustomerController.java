7dba3155bdb934282b579b40056957e6ce14024e
Riham Katout <Riham.Katout@exalt.ps>
2024-08-13 14:38:16 +0300

    use @InitBinder to trim whitespaces

--- src/main/java/com/exalt/training/controller/CustomerController.java
+++ src/main/java/com/exalt/training/controller/CustomerController.java
@@ -2,15 +2,24 @@ package com.exalt.training.controller;
 
 import com.exalt.training.entity.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, trimmerEditor);
+    }
 
     @GetMapping("/customerForm")
     public String showForm(Model model){
