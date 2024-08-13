be534a2d3be29f1ccdf932c493d0d3f848da34b2
Riham Katout <Riham.Katout@exalt.ps>
2024-08-13 14:26:53 +0300

    adding request to DemoController

--- src/main/java/com/exalt/training/controller/DemoController.java
+++ src/main/java/com/exalt/training/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.exalt.training.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,11 @@ public class DemoController {
     @GetMapping("/devTools")
     public String devToolsTest(){
         return "devTools is a good man :)";
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(Model model){
+        model.addAttribute("theDate", new java.util.Date());
+        return "helloword"; // --> src/main/resources/templates/helloworld.html
     }
 }
