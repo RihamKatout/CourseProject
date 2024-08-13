98fb53f3c62aa2551a0204b4e23a60288ab5ae12
Riham Katout <Riham.Katout@exalt.ps>
2024-08-13 14:05:57 +0300

    add all CRUD operations

--- src/main/java/com/exalt/training/controller/EmployeeRestController.java
+++ src/main/java/com/exalt/training/controller/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.exalt.training.dao.EmployeeDAO;
 import com.exalt.training.entity.Employee;
 import com.exalt.training.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +29,33 @@ public class EmployeeRestController {
     public List<Employee> getAllEmployees(){
 //        return employeeDAO.findALL();
         return employeeService.findAll();
+    }
+
+    @GetMapping("/{employeeId}")
+    public Employee getEmployeeById(@PathVariable int employeeId){
+        return employeeService.findById(employeeId);
+    }
+
+    @PostMapping("")
+    public Employee addEmployee(@RequestBody Employee employee){
+
+        // just in case they passed an id, set the id to 0 to force a save instead of update
+        employee.setId(0);
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public String deleteEmployeeById(@PathVariable int employeeId){
+        try{
+            employeeService.deleteById(employeeId);
+            return "Employee was deleted successfully.";
+        }catch (Exception e){
+            return ("Employee Id is not found: "+employeeId);
+        }
     }
 }
