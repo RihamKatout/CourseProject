7b1842ec9e357a0500ec5e32afef8f829f9145f3
Riham Katout <Riham.Katout@exalt.ps>
2024-08-13 14:37:41 +0300

    test validation using html form

---
+++ src/main/java/com/exalt/training/controller/CustomerController.java
@@ -0,0 +1,29 @@
+package com.exalt.training.controller;
+
+import com.exalt.training.entity.Customer;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/customerForm")
+    public String showForm(Model model){
+        model.addAttribute("customer", new Customer());
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute ("customer") Customer customer,
+                              BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return "customer-form";
+        return "customer-confirmation";
+
+    }
+}
