020cfc701aaef862a65c026621d21f46fc3d379e
rihamkatout <rihamkatm@gmail.com>
2024-08-20 13:16:49 +0300

    chore: adding beforeAdvice

---
+++ src/main/java/com/exalt/training/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,15 @@
+package com.exalt.training.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+    @Before("execution(public void addAccount())")
+    public void beforeAddAccountAdvice(){
+        System.out.println("\n ==> Hi before adding account :p");
+    }
+}
