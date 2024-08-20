8a74298ee813a860b07d99f1cf680f114d966bca
rihamkatout <rihamkatm@gmail.com>
2024-08-20 14:24:32 +0300

    Around Advice

--- src/main/java/com/exalt/training/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/exalt/training/aspect/MyDemoLoggingAspect.java
@@ -1,13 +1,12 @@
 package com.exalt.training.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 
 @Aspect
 @Component
@@ -18,6 +17,15 @@ public class MyDemoLoggingAspect {
     @Before("beforeAdding()")
     public void beforeAddAccountAdvice(){
         System.out.println("\n ==> Hi before adding account :p");
+    }
+
+    @Around("beforeAdding()")
+    public Object aroundAddAccountAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        long begin = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        long end = System.currentTimeMillis();
+        System.out.println("=====> duration = " + (end - begin));
+        return result;
     }
 
     @AfterReturning(pointcut = "execution(java.util.List<String> findAccounts())", returning = "result")
