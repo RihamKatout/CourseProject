7eb2a7f0cae6c0cfcee972c3a2cf0e3c73655f97
rihamkatout <rihamkatm@gmail.com>
2024-08-20 13:30:33 +0300

    chore: adding Pointcut to reuse it

--- src/main/java/com/exalt/training/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/exalt/training/aspect/MyDemoLoggingAspect.java
@@ -2,13 +2,16 @@ package com.exalt.training.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+    @Pointcut("execution(public void addAccount())")
+    private void beforeAdding(){}
 
-    @Before("execution(public void addAccount())")
+    @Before("beforeAdding()")
     public void beforeAddAccountAdvice(){
         System.out.println("\n ==> Hi before adding account :p");
     }
